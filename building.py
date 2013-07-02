07c0f198d26153581fa1b3c8da6df9492fc69970
John Weachock <jweachock@gmail.com>
2013-07-02 00:59:59 -0400

    Changed `requires` syntax to use `*args` instead of a list; removed `creates` argument

--- building.py
+++ building.py
@@ -12,7 +12,6 @@ LOCALE = {
 	'abort_no_buildfile': 'abort  \t- no build.py found',
 	'require': 'require\t{}',
 	'help_command': '{}\t- {}',
-	'help_creates': '\t- creates {}',
 	'help_requires': '\t- requires {}',
 	'help_unknown': 'unknown command: {}',
 }
@@ -20,17 +19,23 @@ LOCALE = {
 LIST = []
 DICT = {}
 
+def _highlight(string, color):
+	if CONFIG['color']:
+		if color < 8:
+			return '\033[{color}m{string}\033[0m'.format(string = string, color = color+30)
+		else:
+			return '\033[{color}m{string}\033[0m'.format(string = string, color = color+82)
+
 class AbortException(Exception):
 	def __init__(self, message):
 		Exception.__init__(self, message)
 
 class Task:
-	def __init__(self, func, requires=[], creates=[]):
+	def __init__(self, func, requires):
 		self.func = func
 		self.name = func.__name__
 		self.help = func.__doc__
 		self.requires = requires
-		self.creates = creates
 
 		self.valid = False
 
@@ -52,32 +57,28 @@ class Task:
 		self.valid = True
 
 	def __repr__(self):
-		return highlight('[' + self.name + ']', CONFIG['color_tasks'])
+		return _highlight('[' + self.name + ']', CONFIG['color_tasks'])
 
 
-def task(requires=[], creates=[]):
+def task(*requires):
 	def wrapper(f):
-		new_task = Task(f, requires, creates)
+		new_task = Task(f, requires)
 		LIST.append(new_task)
 		DICT[new_task.name] = new_task
 		return new_task
 
 	return wrapper
 
-def highlight(string, color):
-	if CONFIG['color']:
-		if color < 8:
-			return '\033[{color}m{string}\033[0m'.format(string = string, color = color+30)
-		else:
-			return '\033[{color}m{string}\033[0m'.format(string = string, color = color+82)
+def shell(command):
+	try:
+		return subprocess.check_output(command)
+	except subprocess.CalledProcessError, ex:
+		return ex
 
 def main(args):
 	if len(args) == 0:
 		for t in LIST:
 			print LOCALE['help_command'].format(t, t.help)
-
-			if t.creates:
-				print LOCALE['help_creates'].format(t.creates)
 
 			if t.requires:
 				print LOCALE['help_requires'].format(t.requires)
