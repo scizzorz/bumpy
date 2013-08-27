eaf5d52acf2cfb8151e3ad9f3800286be9c811bc
John Weachock <jweachock@gmail.com>
2013-08-27 15:52:45 -0400

    Use .rst instead of .md for PyPI

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
 	name='bumpy',
-	version='0.1.1',
+	version='0.1.2',
 	description='Create build files and CLI tools easily.',
-	long_description=open('README.md').read(),
+	long_description=open('README.rst').read(),
 	url='http://github.com/scizzorz/bumpy',
 	license='MIT',
 	author='John Weachock',
