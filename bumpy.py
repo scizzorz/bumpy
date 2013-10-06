5628f8826584925cf3cbc2fc901c8de65d26d4f2
John Weachock <jweachock@gmail.com>
2013-10-06 18:00:19 -0400

    Better version string

--- bumpy.py
+++ bumpy.py
@@ -1,6 +1,7 @@
 import getopt, inspect, os, subprocess, time, traceback
 
-__version__ = '0.4.0'
+__version_info__ = (0, 4, 1)
+__version__ = '.'.join(map(str, __version_info__))
 
 # Configuration settings
 CONFIG = {
